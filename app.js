6813dcfa1357c11aee99d6dd038267b798f57b98
AlexJunge <Alex.p.junge@web.de>
2014-10-20 16:29:02 +0200

    erster Node Server

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hallo Welt');
+var http = require('http');
+
+var server = http.createServer(function (req, res){
+
+	res.writeHead(200, {
+	'content-type':'text/plain'
+});
+
+res.write('Hallo Marcel! Nicht verzweifeln!!!');
+
+
+
+res.end();
+
+}).listen(1337);
+
